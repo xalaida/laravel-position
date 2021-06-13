581a43fed3c1e0eca6948074e218f1f39c06fc4c
nevadskiy <nevadskiy@gmail.com>
2021-06-13 14:27:48 +0300

    Add app test case

---
+++ tests/TestCase.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Nevadskiy\Position\Tests;
+
+use Illuminate\Foundation\Application;
+use Orchestra\Testbench\TestCase as OrchestraTestCase;
+
+class TestCase extends OrchestraTestCase
+{
+    /**
+     * Setup the test environment.
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->loadMigrationsFrom(__DIR__.'/Support/Migrations');
+
+        $this->artisan('migrate', ['--database' => 'testbench'])->run();
+    }
+
+    /**
+     * Get package providers.
+     *
+     * @param Application $app
+     */
+    protected function getPackageProviders($app): array
+    {
+        return [];
+    }
+
+    /**
+     * Define environment setup.
+     *
+     * @param Application $app
+     */
+    protected function getEnvironmentSetUp($app): void
+    {
+        $app['config']->set('database.default', 'testbench');
+        $app['config']->set('database.connections.testbench', [
+            'driver' => 'sqlite',
+            'database' => ':memory:',
+            'prefix' => '',
+        ]);
+    }
+}
