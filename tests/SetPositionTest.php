68a9f1ebc2c3bb6eae1e212f8f91646577d0e057
nevadskiy <nevadskiy@gmail.com>
2021-06-13 14:32:34 +0300

    Add test to position in model sequence

--- tests/SetPositionTest.php
+++ tests/SetPositionTest.php
@@ -13,4 +13,14 @@ class SetPositionTest extends TestCase
 
         self::assertEquals(0, $category->position);
     }
+
+    /** @test */
+    public function it_sets_next_position_value_in_model_sequence(): void
+    {
+        $category0 = CategoryFactory::new()->create();
+        $category1 = CategoryFactory::new()->create();
+        $category2 = CategoryFactory::new()->create();
+
+        self::assertEquals(2, $category2->position);
+    }
 }
